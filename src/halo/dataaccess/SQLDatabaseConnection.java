e9e342cc3eee226622184860b46559c325616042
Phan Hieu <Phan Hieu@DESKTOP-DRPS7VO>
2017-03-19 14:39:21 +0700

    init

--- src/halo/dataaccess/SQLDatabaseConnection.java
+++ src/halo/dataaccess/SQLDatabaseConnection.java
@@ -12,8 +12,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
