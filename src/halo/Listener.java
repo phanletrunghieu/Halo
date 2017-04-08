fb0bfa879aec846f1b1f410165a196802551bc2a
Phan Hieu <Phan Hieu@DESKTOP-DRPS7VO>
2017-04-08 18:54:11 +0700

    Đổi hình thức gửi nhận tin nhắn

--- src/halo/Listener.java
+++ src/halo/Listener.java
@@ -1,8 +1,10 @@
 package halo;
 
+import halo.models.Packet;
 import halo.models.User;
 import halo.ui.ChatForm;
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -20,33 +22,38 @@ import java.util.logging.Logger;
 
 /**
  * accept connect request from other users
+ *
  * @author Phan Hieu
  */
 public class Listener extends Thread {
-    
-    private static ArrayList<ChatForm> usersChatting=new ArrayList<>();
-    public static void addUserChatting(ChatForm chatForm){
+
+    private static ArrayList<ChatForm> usersChatting = new ArrayList<>();
+
+    public static void addUserChatting(ChatForm chatForm) {
         usersChatting.add(chatForm);
     }
-    public static void removeUserChatting(ChatForm chatForm){
+
+    public static void removeUserChatting(ChatForm chatForm) {
         usersChatting.remove(chatForm);
     }
-    
+
     private InetAddress inetAddress;
-    
+
     private ServerSocket serverSocket;
     private int port;
-    
+
     public InetAddress getInetAddress() {
         return inetAddress;
     }
+
     public void setInetAddress(InetAddress inetAddress) {
         this.inetAddress = inetAddress;
     }
-    
+
     public int getPort() {
         return port;
     }
+
     public void setPort(int port) {
         this.port = port;
     }
@@ -54,30 +61,30 @@ public class Listener extends Thread {
     public Listener() {
         try {
             ArrayList<InetAddress> iPs = getIPs();
-            inetAddress=iPs.get(0);
-            
-            serverSocket=new ServerSocket(0, 100, inetAddress);
-            port=serverSocket.getLocalPort();
+            inetAddress = iPs.get(0);
+
+            serverSocket = new ServerSocket(0, 100, inetAddress);
+            port = serverSocket.getLocalPort();
         } catch (IOException ex) {
             Logger.getLogger(Listener.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    public Listener(InetAddress inetAddress){
+
+    public Listener(InetAddress inetAddress) {
         try {
-            this.inetAddress=inetAddress;
-            
-            serverSocket=new ServerSocket(0, 100, inetAddress);
+            this.inetAddress = inetAddress;
+
+            serverSocket = new ServerSocket(0, 100, inetAddress);
             System.out.println(serverSocket.getInetAddress().getHostAddress());
-            port=serverSocket.getLocalPort();
+            port = serverSocket.getLocalPort();
         } catch (IOException ex) {
             Logger.getLogger(Listener.class.getName()).log(Level.SEVERE, null, ex);
         }
     }
-    
-    private ArrayList<InetAddress> getIPs() throws SocketException{
+
+    private ArrayList<InetAddress> getIPs() throws SocketException {
         ArrayList<InetAddress> ips = new ArrayList<>();
-        
+
         Enumeration<NetworkInterface> e = NetworkInterface.getNetworkInterfaces();
         while (e.hasMoreElements()) {
             NetworkInterface n = e.nextElement();
@@ -97,36 +104,82 @@ public class Listener extends Thread {
     public void run() {
         Socket clientSocket;
         try {
-            while ((clientSocket=serverSocket.accept())!=null) {
-                InputStream is=clientSocket.getInputStream();
-                BufferedReader br=new BufferedReader(new InputStreamReader(is));
-                String line=br.readLine();
+            while ((clientSocket = serverSocket.accept()) != null) {
+                InputStream is = clientSocket.getInputStream();
+                DataInputStream din = new DataInputStream(clientSocket.getInputStream());
+                byte[] initilize = new byte[1];
+                din.read(initilize, 0, initilize.length);
+                if (initilize[0] == 2) {
+                    int b = 0;
+                    String fromUsername = "";
+                    while ((b = din.read()) != Packet.SEPARATOR) {
+                        fromUsername += (char) b;
+                    }
+
+                    byte[] cmd_buff = new byte[3];
+                    din.read(cmd_buff, 0, cmd_buff.length);
+                    switch (new String(cmd_buff)) {
+                        case Packet.COMMAND_SENDTEXT:
+                            try {
+                                String message = new String(Packet.ReadStream(din));
+                                ShowChatForm(fromUsername, message);
+                            } catch (NullPointerException ex) {
+                                System.out.println("Message is empty");
+                            }
+                            break;
+                    }
+                }
+
                 
-                if(line!=null){
+                /*
+                BufferedReader br = new BufferedReader(new InputStreamReader(is));
+                String line = br.readLine();
+
+                if (line != null) {
                     String[] data = line.split(":");
-                    
-                    int vt=-1;
+
+                    int vt = -1;
                     for (int i = 0; i < usersChatting.size(); i++) {
                         ChatForm chatForm = usersChatting.get(i);
                         if (chatForm.getUser().getUserName().equals(data[0])) {
-                            vt=i;
+                            vt = i;
                         }
                     }
-                    
+
                     ChatForm chatForm;
-                    if(vt==-1){
-                        chatForm=new ChatForm(new User(data[0]));
+                    if (vt == -1) {
+                        chatForm = new ChatForm(new User(data[0]));
                         usersChatting.add(chatForm);
                     } else {
-                        chatForm=usersChatting.get(vt);
+                        chatForm = usersChatting.get(vt);
                     }
                     chatForm.setVisible(true);
                     chatForm.receiveNewMessage(data[1]);
-                }
+                }*/
                 //String ip=(((InetSocketAddress) clientSocket.getRemoteSocketAddress()).getAddress()).toString().replace("/","");
             }
         } catch (IOException | SQLException ex) {
             Logger.getLogger(Listener.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    private void ShowChatForm(String username, String message) throws SQLException {
+        int vt = -1;
+        for (int i = 0; i < usersChatting.size(); i++) {
+            ChatForm chatForm = usersChatting.get(i);
+            if (chatForm.getUser().getUserName().equals(username)) {
+                vt = i;
+            }
+        }
+
+        ChatForm chatForm;
+        if (vt == -1) {
+            chatForm = new ChatForm(new User(username));
+            usersChatting.add(chatForm);
+        } else {
+            chatForm = usersChatting.get(vt);
+        }
+        chatForm.setVisible(true);
+        chatForm.receiveNewMessage(message);
     }
 }
--- src/halo/models/Packet.java
+++ src/halo/models/Packet.java
@@ -3,8 +3,7 @@ package halo.models;
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import java.nio.file.AccessDeniedException;
 
 /**
  *
@@ -12,70 +11,68 @@ import java.util.logging.Logger;
  */
 public class Packet {
 
-    private static byte INITITALIZE = 2;
-    private static byte SEPARATOR = 4;
+    public static byte INITITALIZE = 2;
+    public static byte SEPARATOR = 4;
 
-    public static String COMMAND_SENDTEXT = "123";
-    public static String COMMAND_SENDFILENAME = "124";
+    public final static String COMMAND_SENDTEXT = "123";
+    public final static String COMMAND_SENDFILENAME = "124";
 
     /**
      *
-     * @param from_username
+     * @param fromUsername
      * @param command 123: gửi text. 124: gửi file name.
      * @param data
      * @return
      */
-    public static byte[] CreateDataPacket(String fromUsername, String command, byte[] data) {
-        byte[] packet = null;
-        try {
-            byte[] initialize = new byte[1];
-            initialize[0] = INITITALIZE;
-            byte[] separator = new byte[1];
-            separator[0] = SEPARATOR;
-            byte[] from_username = fromUsername.getBytes("UTF8");
-            byte[] cmd = command.getBytes("UTF8");
-            byte[] data_length = String.valueOf(data.length).getBytes("UTF8");
-            packet = new byte[initialize.length + from_username.length + separator.length + cmd.length + data_length.length + separator.length + data.length];
-
-            int packet_offset = 0;
-            System.arraycopy(initialize, 0, packet, packet_offset, initialize.length);
-            packet_offset += initialize.length;
-            System.arraycopy(from_username, 0, packet, packet_offset, from_username.length);
-            packet_offset += from_username.length;
-            System.arraycopy(separator, 0, packet, packet_offset, separator.length);
-            packet_offset += separator.length;
-            System.arraycopy(cmd, 0, packet, packet_offset, cmd.length);
-            packet_offset += cmd.length;
-            System.arraycopy(data_length, 0, packet, packet_offset, data_length.length);
-            packet_offset += data_length.length;
-            System.arraycopy(separator, 0, packet, packet_offset, separator.length);
-            packet_offset += separator.length;
-            System.arraycopy(data, 0, packet, packet_offset, data.length);
-            //packet_offset+=data.length;
-        } catch (UnsupportedEncodingException ex) {
-            Logger.getLogger(Packet.class.getName()).log(Level.SEVERE, null, ex);
+    public static byte[] CreateDataPacket(String fromUsername, String command, byte[] data) throws UnsupportedEncodingException, IOException {
+        if (data.length == 0) {
+            throw new IOException();
         }
+
+        byte[] initialize = new byte[1];
+        initialize[0] = INITITALIZE;
+        byte[] separator = new byte[1];
+        separator[0] = SEPARATOR;
+        byte[] from_username = fromUsername.getBytes("UTF8");
+        byte[] cmd = command.getBytes("UTF8");
+        byte[] data_length = String.valueOf(data.length).getBytes("UTF8");
+        byte[] packet = new byte[initialize.length + from_username.length + separator.length + cmd.length + data_length.length + separator.length + data.length];
+
+        int packet_offset = 0;
+        System.arraycopy(initialize, 0, packet, packet_offset, initialize.length);
+        packet_offset += initialize.length;
+        System.arraycopy(from_username, 0, packet, packet_offset, from_username.length);
+        packet_offset += from_username.length;
+        System.arraycopy(separator, 0, packet, packet_offset, separator.length);
+        packet_offset += separator.length;
+        System.arraycopy(cmd, 0, packet, packet_offset, cmd.length);
+        packet_offset += cmd.length;
+        System.arraycopy(data_length, 0, packet, packet_offset, data_length.length);
+        packet_offset += data_length.length;
+        System.arraycopy(separator, 0, packet, packet_offset, separator.length);
+        packet_offset += separator.length;
+        System.arraycopy(data, 0, packet, packet_offset, data.length);
+        //packet_offset+=data.length;
         return packet;
     }
 
-    public static byte[] ReadStream(DataInputStream din) {
+    public static byte[] ReadStream(DataInputStream din) throws IOException, NullPointerException  {
         byte[] data_buff = null;
-        try {
-            int b = 0;
-            String buff_length = "";
-            while ((b = din.read()) != SEPARATOR) {
-                buff_length += (char) b;
-            }
-            int data_length = Integer.parseInt(buff_length);
-            data_buff = new byte[Integer.parseInt(buff_length)];
-            int byte_read = 0;
-            int byte_offset = 0;
-            while (byte_offset < data_length) {
-                byte_read = din.read(data_buff, byte_offset, data_length - byte_offset);
-                byte_offset += byte_read;
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Packet.class.getName()).log(Level.SEVERE, null, ex);
+        int b = 0;
+        String buff_length = "";
+        while ((b = din.read()) != SEPARATOR) {
+            buff_length += (char) b;
+        }
+        int data_length = Integer.parseInt(buff_length);
+        if(data_length==0)
+            throw new NullPointerException();
+        
+        data_buff = new byte[Integer.parseInt(buff_length)];
+        int byte_read = 0;
+        int byte_offset = 0;
+        while (byte_offset < data_length) {
+            byte_read = din.read(data_buff, byte_offset, data_length - byte_offset);
+            byte_offset += byte_read;
         }
         return data_buff;
     }
