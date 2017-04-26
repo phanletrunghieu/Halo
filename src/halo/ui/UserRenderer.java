1191b6e0e4deb82b7bac3093b9c5461344159061
15520237 <15520237@gm.uit.edu.vn>
2017-04-26 23:52:41 +0700

    update friend list renderer

--- src/halo/ui/UserRenderer.java
+++ src/halo/ui/UserRenderer.java
@@ -7,6 +7,13 @@ package halo.ui;
 
 import halo.models.User;
 import java.awt.Component;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -19,13 +26,22 @@ import javax.swing.ListCellRenderer;
 public class UserRenderer extends JLabel implements ListCellRenderer<User> {
 
     public UserRenderer() {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        
     }
 
     @Override
     public Component getListCellRendererComponent(JList<? extends User> list, User user, int index, boolean isSelected, boolean cellHasFocus) {
-        ImageIcon icon = new ImageIcon((user.getAvatar()));
-        this.setIcon(icon);
+        //ImageIcon icon = new ImageIcon((user.getAvatar()));
+        //this.setIcon(icon);
+        if (user.getAvatar() != null && user.getAvatar().length > 0) {
+            try {
+                BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(user.getAvatar()));
+                Image newimg = bufferedImage.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+                this.setIcon(new ImageIcon(newimg));
+            } catch (IOException ex) {
+                Logger.getLogger(FriendList.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
         String onlineStatus = "";
         if(user.isIsOnline()){
             onlineStatus = "Is online";
