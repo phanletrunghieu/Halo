71775d6c9255d925e341d9d54b69430f794e64e9
15520237 <15520237@gm.uit.edu.vn>
2017-05-13 11:50:47 +0700

    add default avatar for users don't have

--- src/halo/ui/UserRenderer.java
+++ src/halo/ui/UserRenderer.java
@@ -25,14 +25,20 @@ import javax.swing.ListCellRenderer;
  */
 public class UserRenderer extends JLabel implements ListCellRenderer<User> {
 
+    private ImageIcon defaultUserIcon;
+
     public UserRenderer() {
-        
+        try {
+            BufferedImage bufferedImage = ImageIO.read(getClass().getResource("/halo/resources/no-user-image-square.jpg"));
+            Image newimg = bufferedImage.getScaledInstance(100, 100, java.awt.Image.SCALE_SMOOTH);
+            defaultUserIcon = new ImageIcon(newimg);
+        } catch (IOException ex) {
+            defaultUserIcon = new ImageIcon();
+        }
     }
 
     @Override
     public Component getListCellRendererComponent(JList<? extends User> list, User user, int index, boolean isSelected, boolean cellHasFocus) {
-        //ImageIcon icon = new ImageIcon((user.getAvatar()));
-        //this.setIcon(icon);
         if (user.getAvatar() != null && user.getAvatar().length > 0) {
             try {
                 BufferedImage bufferedImage = ImageIO.read(new ByteArrayInputStream(user.getAvatar()));
@@ -41,18 +47,21 @@ public class UserRenderer extends JLabel implements ListCellRenderer<User> {
             } catch (IOException ex) {
                 Logger.getLogger(FriendList.class.getName()).log(Level.SEVERE, null, ex);
             }
+        } else {
+            this.setIcon(defaultUserIcon);
         }
+
         String onlineStatus = "";
-        if(user.isIsOnline()){
+        if (user.isIsOnline()) {
             onlineStatus = "Online";
         } else {
             onlineStatus = "Offline";
         }
         String status = "";
-        if(user.getStatus() != null){
+        if (user.getStatus() != null) {
             status = user.getStatus();
         }
-        this.setText( "<html>" + user.getUserName() + "<br/>" + onlineStatus + "<br/>" + status + "</html>");
+        this.setText("<html>" + user.getUserName() + "<br/>" + onlineStatus + "<br/>" + status + "</html>");
         return this;
     }
 
